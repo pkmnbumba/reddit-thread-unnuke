a2a5ebca0be89a175a8141819933ebd52aaed2ac
pkmnbumba <pkmnbumba@gmail.com>
2017-07-11 18:10:01 -0700

    temp test

--- nukeThread.js
+++ nukeThread.js
@@ -64,7 +64,6 @@ function deepRemove (content, preserveDistinguished) {
 function deepApprove (content, preserveRemoved) {
   var replies = content.comments || content.replies;
   console.log(content.banned_by['name']);
-  console.log(cachedRequester.getMe());
   var determineRemoved = preserveRemoved;
   var approveCurrentItem = !content.removed || content.banned_by === null
     ? Promise.resolve()
