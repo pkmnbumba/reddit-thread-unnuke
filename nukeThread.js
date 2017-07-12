f756b4d6cc5148660ff9638c02213a82b7490f82
pkmnbumba <pkmnbumba@gmail.com>
2017-07-11 18:11:34 -0700

    null fix

--- nukeThread.js
+++ nukeThread.js
@@ -63,7 +63,9 @@ function deepRemove (content, preserveDistinguished) {
 
 function deepApprove (content, preserveRemoved) {
   var replies = content.comments || content.replies;
-  console.log(content.banned_by['name']);
+  if (content.banned_by !== null) {
+    console.log(content.banned_by['name']);
+  }
   var determineRemoved = preserveRemoved;
   var approveCurrentItem = !content.removed || content.banned_by === null
     ? Promise.resolve()
